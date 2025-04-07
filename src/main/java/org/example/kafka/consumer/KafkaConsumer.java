1377287eb234312dfa1142029d3c3cf7e00931ba
jennifer <cxxyao2@gmail.com>
2025-04-07 14:12:30 -0400

    string serializer finished

---
+++ src/main/java/org/example/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package org.example.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "janetopic", groupId = "myGroup")
+    public void consumeMessage(String msg) {
+        log.info(format("Consuming the message from server:: %s",msg));
+
+    }
+}
---
+++ src/main/java/org/example/kafka/producer/KafkaProducer.java
@@ -0,0 +1,21 @@
+package org.example.kafka.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        log.info(format("Sending Message to janetopic :: %s", message));
+        kafkaTemplate.send("janetopic", message);
+    }
+}
